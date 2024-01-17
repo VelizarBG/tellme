fac407512ee3f6411f5d5578dfad71fedd2fe301
Velizar <31567122+VelizarBG@users.noreply.github.com>
2024-01-18 00:31:50 +0200

    Fix forced registry sync by FAPI

--- src/main/java/fi/dy/masa/tellme/command/CommandTellMe.java
+++ src/main/java/fi/dy/masa/tellme/command/CommandTellMe.java
@@ -2,8 +2,12 @@ package fi.dy.masa.tellme.command;
 
 import java.util.Collections;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
+import eu.pb4.polymer.rsm.api.RegistrySyncUtils;
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
+import net.minecraft.command.argument.serialize.ArgumentSerializer;
 import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
@@ -29,13 +33,19 @@ public class CommandTellMe
 
     public static void registerArgumentTypes()
     {
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:biome"), BiomeArgument.class, ConstantArgumentSerializer.of(BiomeArgument::create));
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:block_grouping"), BlockStateCountGroupingArgument.class, ConstantArgumentSerializer.of(BlockStateCountGroupingArgument::create));
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:file"), FileArgument.class, ConstantArgumentSerializer.of(FileArgument::createEmpty));
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:grouping"), GroupingArgument.class, ConstantArgumentSerializer.of(GroupingArgument::create));
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:output_format"), OutputFormatArgument.class, ConstantArgumentSerializer.of(OutputFormatArgument::create));
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:output_type"), OutputTypeArgument.class, ConstantArgumentSerializer.of(OutputTypeArgument::create));
-        ArgumentTypeRegistry.registerArgumentType(new Identifier("tellme:string_collection"), StringCollectionArgument.class, ConstantArgumentSerializer.of(() -> StringCollectionArgument.create(Collections::emptyList, "")));
+        registerServerArgumentType(new Identifier("tellme:biome"), BiomeArgument.class, ConstantArgumentSerializer.of(BiomeArgument::create));
+        registerServerArgumentType(new Identifier("tellme:block_grouping"), BlockStateCountGroupingArgument.class, ConstantArgumentSerializer.of(BlockStateCountGroupingArgument::create));
+        registerServerArgumentType(new Identifier("tellme:file"), FileArgument.class, ConstantArgumentSerializer.of(FileArgument::createEmpty));
+        registerServerArgumentType(new Identifier("tellme:grouping"), GroupingArgument.class, ConstantArgumentSerializer.of(GroupingArgument::create));
+        registerServerArgumentType(new Identifier("tellme:output_format"), OutputFormatArgument.class, ConstantArgumentSerializer.of(OutputFormatArgument::create));
+        registerServerArgumentType(new Identifier("tellme:output_type"), OutputTypeArgument.class, ConstantArgumentSerializer.of(OutputTypeArgument::create));
+        registerServerArgumentType(new Identifier("tellme:string_collection"), StringCollectionArgument.class, ConstantArgumentSerializer.of(() -> StringCollectionArgument.create(Collections::emptyList, "")));
+    }
+
+    private static <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> void registerServerArgumentType(
+        Identifier id, Class<? extends A> clazz, ArgumentSerializer<A, T> serializer) {
+        ArgumentTypeRegistry.registerArgumentType(id, clazz, serializer);
+        RegistrySyncUtils.setServerEntry(Registries.COMMAND_ARGUMENT_TYPE, id);
     }
 
     protected static void register(CommandDispatcher<ServerCommandSource> dispatcher, String baseCommandName, final int permissionLevel)
